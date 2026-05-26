6428c1f59f4f51f3d56267b37efe37d070f37723
jmaria69 <jmaria.romero@hotmail.com>
2026-05-26 12:01:10 +0200

    Vercel redeploy for GA configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
-﻿const nextConfig = {
-  /* config options here */
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://www.gstatic.com; font-src 'self' https://fonts.gstatic.com data:;",
+          },
+        ],
+      },
+    ];
+  },
 };
+
 module.exports = nextConfig;
